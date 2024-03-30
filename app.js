6312e651065d498d6392eae750a8e98ab2b7fe41
Yassin-010 <YassinSE898@gmail.com>
2024-03-30 10:36:05 +0000

    lab07 done

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ Employee.prototype.calculateSalary = function () {
 
     const { min, max } = salaryTable[this.level];
     this.salary = Math.floor(Math.random() * (max - min + 1)) + min;
+
+
     this.netSalary = this.salary - (this.salary * 7.5) / 100;
 };
 
@@ -29,23 +31,49 @@ Employee.prototype.render = function () {
     const employeeName = document.createElement("h3");
     employeeName.textContent = this.fullName;
 
+    const employeeID = document.createElement("p");
+    employeeID.textContent = `Employee ID: ${this.employeeID}`;
+
+    const employeeDepartment = document.createElement("p");
+    employeeDepartment.textContent = `Department: ${this.department}`;
+
+    const employeeLevel = document.createElement("p");
+    employeeLevel.textContent = `Level: ${this.level}`;
+
     const employeeSalary = document.createElement("p");
     employeeSalary.textContent = `Salary: $${this.salary.toFixed(2)}`;
 
+    const employeeNetSalary = document.createElement("p");
+    employeeNetSalary.textContent = `Net Salary: $${this.netSalary.toFixed(2)}`;
+
+    const employeeImage = document.createElement("img");
+    employeeImage.src = this.imageURL;
+    employeeImage.alt = this.fullName;
+    employeeImage.height = "100"
+    employeeImage.width = "100"
+
+
+
+
+    employeeCard.appendChild(employeeImage);
     employeeCard.appendChild(employeeName);
+    employeeCard.appendChild(employeeID);
+    employeeCard.appendChild(employeeDepartment);
+    employeeCard.appendChild(employeeLevel);
     employeeCard.appendChild(employeeSalary);
+    employeeCard.appendChild(employeeNetSalary);
 
     mainSection.appendChild(employeeCard);
 };
 
 const employees = [
-    new Employee(1000, "Ghazi Samer", "Administration", "Senior", "image-url"),
-    new Employee(1001, "Lana Ali", "Finance", "Senior", "image-url"),
-    new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "image-url"),
-    new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", "image-url"),
-    new Employee(1004, "Omar Zaid", "Development", "Senior", "image-url"),
-    new Employee(1005, "Rana Saleh", "Development", "Junior", "image-url"),
-    new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "image-url"),
+    new Employee(1000, "Ghazi Samer", "Administration", "Senior", "https://static.vecteezy.com/system/resources/previews/004/274/186/original/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg"),
+    new Employee(1001, "Lana Ali", "Finance", "Senior", "https://static.vecteezy.com/system/resources/previews/004/274/186/original/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg"),
+    new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "https://static.vecteezy.com/system/resources/previews/004/274/186/original/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg"),
+    new Employee(1003, "Safi Walid", "Administration", "Mid-Senior", "https://static.vecteezy.com/system/resources/previews/004/274/186/original/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg"),
+    new Employee(1004, "Omar Zaid", "Development", "Senior", "https://static.vecteezy.com/system/resources/previews/004/274/186/original/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg"),
+    new Employee(1005, "Rana Saleh", "Development", "Junior", "https://static.vecteezy.com/system/resources/previews/004/274/186/original/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg"),
+    new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "https://static.vecteezy.com/system/resources/previews/004/274/186/original/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg"),
 ];
 
 employees.forEach((employee) => {
